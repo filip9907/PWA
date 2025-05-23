c09c57e3e4493eec4cb993e126902c5c3bc5a97c
filip9907 <56163351+filip9907@users.noreply.github.com>
2025-05-23 20:47:47 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,22 @@
 const CACHE_NAME = 'auth-pwa-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  '/PWA/',
+  '/PWA/index.html',
+  '/PWA/style.css',
+  '/PWA/script.js',
+  '/PWA/manifest.json',
+  '/PWA/icons/icon-192.png',
+  '/PWA/icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });

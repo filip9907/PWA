bebf4140dd8a1e6369446deea781636ab2a9bda4
filip9907 <56163351+filip9907@users.noreply.github.com>
2025-05-23 15:18:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
 document.getElementById("registerForm").addEventListener("submit", async function (e) {
   e.preventDefault();
   const username = document.getElementById("regUsername").value;
   const password = document.getElementById("regPassword").value;
-  const res = await fetch("https://localhost:3000/register", {
+  const res = await fetch("https://pwabackend-production.up.railway.app/register", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password }),
@@ -15,7 +14,7 @@ document.getElementById("loginForm").addEventListener("submit", async function (
   e.preventDefault();
   const username = document.getElementById("loginUsername").value;
   const password = document.getElementById("loginPassword").value;
-  const res = await fetch("https://localhost:3000/login", {
+  const res = await fetch("https://pwabackend-production.up.railway.app/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username, password }),
